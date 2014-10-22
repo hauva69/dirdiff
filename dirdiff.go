6daed3c00d0daa3e68841a0fcc60e312a994f004
Ari Makela <ari.makela@kauppalehti.fi>
2014-10-22 22:24:27 +0300

    Some work done

--- dirdiff.go
+++ dirdiff.go
@@ -8,16 +8,19 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	//	"strings"
 )
 
 var mapDn1 = make(map[string]string)
 var mapDn2 = make(map[string]string)
+var mapDn map[string]string
 
 func getMd5Sum(path string, info os.FileInfo) string {
 	const fileChunk = 8192
 	file, err := os.Open(path)
 	if err != nil {
-		log.Fatalf("%s", err.Error())
+		log.Printf("%s", err.Error())
+		return "ERROR"
 	}
 	defer file.Close()
 	blocks := uint64(math.Ceil(float64(info.Size()) / float64(fileChunk)))
@@ -37,20 +40,47 @@ func getMd5Sum(path string, info os.FileInfo) string {
 func walkFunc(path string, info os.FileInfo, err error) error {
 	if err != nil {
 		return err
-	} else if path == ".git" {
-		return filepath.SkipDir
 	}
-	fmt.Println(path)
-	mapDn1[path] = getMd5Sum(path, info)
-	fmt.Println(mapDn1[path])
+	//else if strings.Index(path, ".git") != -1 {
+	//		return filepath.SkipDir
+	//	}
+
+	if !info.IsDir() {
+		mapDn[path] = getMd5Sum(path, info)
+	}
 
 	return nil
 }
 
 func main() {
 	dn1 := os.Args[1]
+	mapDn = mapDn1
 	err := filepath.Walk(dn1, walkFunc)
 	if err != nil {
 		log.Fatalf("%v\n", err)
+	}
+
+	dn2 := os.Args[2]
+	mapDn = mapDn2
+	err = filepath.Walk(dn2, walkFunc)
+	if err != nil && err != filepath.SkipDir {
+		log.Printf("%v\n", err)
+	}
+
+	for k, _ := range mapDn1 {
+		fmt.Println(k)
+	}
+
+	for k, _ := range mapDn2 {
+		fmt.Println(k)
+	}
+
+	for k, v := range mapDn1 {
+		_, ok := mapDn2[dn2+k]
+		if ok {
+			fmt.Printf("%s=%s\n", k, v)
+		} else {
+			fmt.Printf("%s does not exist in %s\n", k, dn2)
+		}
 	}
 }
